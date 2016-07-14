3cff1dcdca32c4179191e1705c79c70d70fd5a83
admin <admin@machine>
2016-07-14 09:13:44 +0200

    removed hostname from config part

--- CalculatorService/CalculatorService.cs
+++ CalculatorService/CalculatorService.cs
@@ -21,47 +21,74 @@ namespace CalculatorService
         public CalculatorService(StatelessServiceContext context)
             : base(context)
         { }
-              
+
         protected override IEnumerable<ServiceInstanceListener> CreateServiceInstanceListeners()
         {
-            return new[] { new ServiceInstanceListener((context) =>
+
+            yield return new ServiceInstanceListener(context =>
             {
-                string host = HostFromConfig(context);
-                if (string.IsNullOrWhiteSpace(host))
-                {
-                     host = context.NodeContext.IPAddressOrFQDN;
-                }
-               
-                var endpointConfig = context.CodePackageActivationContext.GetEndpoint("CalculatorEndpoint");
-                int port= endpointConfig.Port;
-                string scheme = endpointConfig.Protocol.ToString();
-                string uri = string.Format(CultureInfo.InvariantCulture, "{0}://{1}:{2}", scheme, host, port);
-
-                var listener = new WcfCommunicationListener<ICalculatorService>(
-                    serviceContext: context,
-                    wcfServiceObject: new WcfCalculatorService(),
-                    listenerBinding: new WebHttpBinding(WebHttpSecurityMode.None),
-                    address : new EndpointAddress(uri)
-                );
-
-                var ep = listener.ServiceHost.Description.Endpoints.First();
-                ep.Behaviors.Add(new WebHttpBehavior());
-
-                return listener;
-            }
-        )};
-
+                return CreateRestListener(context);
+                //return CreateSoapListener(context);
+            });          
         }
 
-        private static string HostFromConfig(StatelessServiceContext context)
+        private static ICommunicationListener CreateRestListener(StatelessServiceContext context)
+        {
+            string host = context.NodeContext.IPAddressOrFQDN;
+            var endpointConfig = context.CodePackageActivationContext.GetEndpoint("CalculatorEndpoint");
+            int port = endpointConfig.Port;
+            string scheme = endpointConfig.Protocol.ToString();
+            string uri = string.Format(CultureInfo.InvariantCulture, "{0}://{1}:{2}/", scheme, host, port);
+            var listener = new WcfCommunicationListener<ICalculatorService>(
+                serviceContext: context,
+                wcfServiceObject: new WcfCalculatorService(),
+                listenerBinding: new WebHttpBinding(WebHttpSecurityMode.None),
+                address: new EndpointAddress(uri)
+            );
+            var ep = listener.ServiceHost.Description.Endpoints.Last();
+            ep.Behaviors.Add(new WebHttpBehavior());
+            return listener;
+        }
+
+        private static ICommunicationListener CreateSoapListener(StatelessServiceContext context)
+        {
+            string host = context.NodeContext.IPAddressOrFQDN;
+            var endpointConfig = context.CodePackageActivationContext.GetEndpoint("CalculatorEndpoint");
+            int port = endpointConfig.Port;
+            string scheme = endpointConfig.Protocol.ToString();
+
+            string uri = string.Format(CultureInfo.InvariantCulture, "{0}://{1}:{2}/", scheme, host, port);
+            var listener = new WcfCommunicationListener<ICalculatorService>(
+                serviceContext: context,
+                wcfServiceObject: new WcfCalculatorService(),
+                listenerBinding: new BasicHttpBinding(BasicHttpSecurityMode.None),
+                address: new EndpointAddress(uri)
+            );
+
+            // Check to see if the service host already has a ServiceMetadataBehavior
+            ServiceMetadataBehavior smb = listener.ServiceHost.Description.Behaviors.Find<ServiceMetadataBehavior>();
+            // If not, add one
+            if (smb == null)
+            {
+                smb = new ServiceMetadataBehavior();
+                smb.MetadataExporter.PolicyVersion = PolicyVersion.Policy15;
+                smb.HttpGetEnabled = true;
+                smb.HttpGetUrl = new Uri(uri);
+
+                listener.ServiceHost.Description.Behaviors.Add(smb);
+            }
+            return listener;
+        }
+
+        private static string EndpointTypeFromConfig(StatelessServiceContext context)
         {
             var configSection = context.CodePackageActivationContext.GetConfigurationPackageObject("Config");
-            var section = (configSection?.Settings.Sections.Contains("ClusterLocation") ?? false) ? configSection?.Settings.Sections["ClusterLocation"] : null;
-            string host = (section?.Parameters.Contains("EndpointUrl") ?? false) ? section.Parameters["EndpointUrl"].Value : null;
-            return host;
+            var section = (configSection?.Settings.Sections.Contains("Endpoint") ?? false) ? configSection?.Settings.Sections["Endpoint"] : null;
+            string endPointType = (section?.Parameters.Contains("EndpointType") ?? false) ? section.Parameters["EndpointType"].Value : null;
+            return endPointType;
         }
     }
-        
+
     internal class WcfCalculatorService : ICalculatorService
     {
         public Task<int> Add(int a, int b)
